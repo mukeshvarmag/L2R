cbc75f3bf95b6eebe9d76f49e1d4683338a867a9
Gugulothu Mukesh Varma <75801524+mukeshvarmag@users.noreply.github.com>
2023-08-17 12:18:32 +0530

    For Loop Added

---
+++ src/main/java/org/example/loop/forLoop.java
@@ -0,0 +1,10 @@
+package org.example.loop;
+
+public class forLoop {
+    public static void main(String[] args){
+        int i = 0;
+        for(i=0;i<10;i++){
+            System.out.println(i);
+        }
+    }
+}
